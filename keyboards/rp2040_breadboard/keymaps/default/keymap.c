aac8ffcd37b3700c04ae3af3b55a0976b041af85
kata0510 <yuchi.kbd@gmail.com>
2023-12-25 09:56:48 +0900

    update 2x4

--- keyboards/rp2040_breadboard/keymaps/default/keymap.c
+++ keyboards/rp2040_breadboard/keymaps/default/keymap.c
@@ -4,15 +4,8 @@
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    /*
-     * ┌───┬───┬───┐
-     * │ A │ B │ C │
-     * ├───┼───┼───┤
-     * │ D │ E │ F │
-     * └───┴───┴───┘
-     */
-    [0] = LAYOUT_ortho_2x3(
-        KC_A,    KC_B,    KC_C,
-        KC_D,    KC_E,    KC_F
+    [0] = LAYOUT_ortho_2x4(
+        KC_1,   KC_2,   KC_3,   KC_4,
+        KC_5,   KC_6,   KC_7,   KC_8
     )
 };
