a2fd38e33add1b0beacd305d29c5a63e99dedae8
kata0510 <yuchi.kbd@gmail.com>
2023-12-29 04:20:47 +0900

    add rp2040_8key

---
+++ keyboards/rp2040_8key/keymaps/default/keymap.c
@@ -0,0 +1,11 @@
+// Copyright 2023 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [0] = LAYOUT_ortho_2x8(
+        KC_1,  KC_2,  KC_3,  KC_4, KC_5,  KC_6,  KC_7,  KC_8,
+        KC_A,  KC_B,  KC_C,  KC_D, KC_E,  KC_F,  KC_G,  KC_H
+    )
+};
